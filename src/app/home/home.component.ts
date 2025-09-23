fd2b310dc269e709df6eda7586c70ea12cb150ea
Alessandro Panarese <darenagel@hotmail.it>
2025-09-23 16:51:29 +0200

    added new method loadAllCourses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareRep
 import { HttpClient } from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import {CoursesService} from "../services/courses.service";
 
 
 @Component({
@@ -20,7 +21,7 @@ export class HomeComponent implements OnInit {
   advancedCourses: Course[];
 
 
-  constructor(private http: HttpClient, private dialog: MatDialog) {
+  constructor(private courseService: CoursesService, private dialog: MatDialog) {
 
   }
 
