5738c6f73d3675447a70576b5a7e5bae69830fa2
Alessandro Panarese <darenagel@hotmail.it>
2025-09-26 13:00:25 +0200

    use observables to load courses

---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,67 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Course} from "../model/course";
+import {AsyncPipe, NgForOf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardTitle
+} from "@angular/material/card";
+import {MatTab} from "@angular/material/tabs";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {CourseDialogComponent} from "../course-dialog/course-dialog.component";
+import {filter} from "rxjs/operators";
+
+@Component({
+  selector: 'courses-card-list',
+  imports: [
+    AsyncPipe,
+    MatButton,
+    MatCard,
+    MatCardActions,
+    MatCardContent,
+    MatCardHeader,
+    MatCardImage,
+    MatCardTitle,
+    MatTab,
+    NgForOf
+  ],
+  templateUrl: './courses-card-list.component.html',
+  styleUrl: './courses-card-list.component.scss'
+})
+export class CoursesCardListComponent implements OnInit{
+
+  @Input() courses: Course[] = [];
+
+  @Output()
+  private courseChanged = new EventEmitter();
+
+  constructor(private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+  }
+
+  editCourse(course: Course) {
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
+
+    dialogConfig.data = course;
+
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed()
+      .pipe(
+        filter(val => !!val)
+      )
+      .subscribe()
+
+  }
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private courseService: CoursesService, private dialog: MatDialog) {
+  constructor(private courseService: CoursesService) {
 
   }
 
@@ -46,19 +46,7 @@ export class HomeComponent implements OnInit {
 
   }
 
-  editCourse(course: Course) {
 
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
-  }
 
 }
 
